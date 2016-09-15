c17d8f0e4ff54dd439f446564c94780af73fe7d7
atpyk <atpyk@163.com>
2016-09-15 11:10:33 +0800

    Helloworld
    This the hello world for .net core

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string a = "ddd";
+
+            Console.WriteLine("Hello {0}", a);
+
+            
+        }
+    }
+}
